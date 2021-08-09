399957ba714b2ab55bc3432832801150fee6190b
Yashaswi Verma <39966586+YASH4ENG@users.noreply.github.com>
2021-08-09 12:56:22 +0100

    Add files via upload

---
+++ variables (2).tf
@@ -0,0 +1,5 @@
+
+variable access_key {} #To take input for your aws access key
+variable secret_key {} #To take input for your aws secret key
+variable region {} # To take input for region in which you want to prvision your infrastructure
+
